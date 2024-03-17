2f6e053b55057d067686ca4891a980f47a3eaa41
KEshav <keshavsingh7744@gmail.com>
2024-03-17 13:44:36 +0530

    commits

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
-
-const {PORT} = require('./config/serverConfig')
+const bodyParser = require("body-parser");
+const {PORT} = require('./config/serverConfig');
 
 
 
 const setupAndStartServer = async()=>{
     // creating the express object
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
     app.listen(PORT,()=>{
         console.log(`server started at port ${PORT}`);
     })
