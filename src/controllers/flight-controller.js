e73cff804814118a52ba06beaeeb588365b7da21
KEshav <keshavsingh7744@gmail.com>
2024-05-09 00:10:00 +0530

    added some custom filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -23,8 +23,30 @@ const create = async (req,res)=>{
       }
 }
 
+const getAll = async(req,res)=>{
+    try {
+        const response = await flightService.getAllFlightData(req.query);
+        return res.status(200).json({
+            data : response , 
+            success : true ,
+            err : {},
+            message : "Successfully fetched the flights",
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "not able to fetch the Flights",
+            err: error,
+          });
+    }
+}
+
 
 
 module.exports ={
     create ,
+    getAll
 }
